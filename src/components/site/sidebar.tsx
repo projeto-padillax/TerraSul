2107c91aeb1a12259e0bc69ac615173bf24424d5
João Vitor Schwingel <jv.schwingel@gmail.com>
2025-09-05 19:35:32 -0300

    arrumando build

--- src/components/site/sidebar.tsx
+++ src/components/site/sidebar.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { X, Menu, CodeXml } from "lucide-react";
+import { X } from "lucide-react";
 import { PaginasConteudo } from "@prisma/client";
 
 interface SidebarProps {
