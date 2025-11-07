4990a2200ff6b317f2fea2b89c754b1d59528fcb
João Vitor Schwingel <jv.schwingel@gmail.com>
2025-11-06 21:05:01 -0300

    robots com url certa

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://terrasulimoveis.vercel.app/sitemap.xml',
+    sitemap: 'https://www.terrasulimoveis.com.br/',
   }
 }
