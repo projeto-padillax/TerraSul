1f606b97f7d1d331594f297be4004e4a1d7295cd
João Vitor Schwingel <jv.schwingel@gmail.com>
2025-12-11 15:30:49 -0300

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ["react", "react-dom"],
-  },
   async headers() {
     return [
       {
