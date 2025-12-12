a9815b2a904442d067797a4721aa1cb860233cc8
João Vitor Schwingel <jv.schwingel@gmail.com>
2025-12-12 09:51:52 -0300

    voltando com o gtm

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -1,24 +1,24 @@
-// 'use client';
+'use client';
 
-// import Script from "next/script";
+import Script from "next/script";
 
-// export default function GoogleTagManager() {
-//   return (
-//     <>
-//       {/* GTM Script */}
-//       <Script
-//         id="google-tag-manager"
-//         strategy="afterInteractive"
-//       >
-//         {`
-//           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//           })(window,document,'script','dataLayer','GTM-5ZF3W4BG');
-//         `}
-//       </Script>
-//     </>
-//   );
-// }
+export default function GoogleTagManager() {
+  return (
+    <>
+      {/* GTM Script */}
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+      >
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5ZF3W4BG');
+        `}
+      </Script>
+    </>
+  );
+}
     
